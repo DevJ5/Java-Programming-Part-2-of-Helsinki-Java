596fc7702312c5b7d51eab39142c1f06d3af7dc8
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-08 17:13:01 +0100

    Smileys finished

--- week7-week7_01.Smileys/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Smileys smiley = new Smileys("Happy");
-
-        System.out.println(smiley.name);
-    }
-}
--- week7-week7_01.Smileys/src/Smileys.java
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Smileys {
-    private String name;
 
-    public Smileys(String name) {
-        this.name = name;
+    public static void printSmileyLine(int length) {
+        int i = 0;
+        while (i < length) {
+            System.out.print(":)");
+            i++;
+        }
+    }
+
+    public static void printCharacterLine(String input, int spaces) {
+        if (spaces == 1) System.out.print(":) " + input + " " + ":)");
+        if (spaces == 2) System.out.print(":) " + input + "  " + ":)");
     }
 
     public static void main(String[] args) {
@@ -11,8 +20,30 @@ public class Smileys {
         // printWithSmileys("Method");
         // printWithSmileys("Beerbottle");
         // printWithSmileys("Interface")
-        // okay;
-    }
+        // Aantal karakters / 2 + 3 bij even. Aantal Math.ceil((double)karakters / 2 + 3 bij odd.
 
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What would you like to surround with smileys?");
+        String input = reader.nextLine();
+        int length = input.length();
+
+        if (length % 2 == 0) {
+            printSmileyLine(length / 2 + 3);
+            System.out.println("");
+            printCharacterLine(input, 1);
+            System.out.println("");
+            printSmileyLine(length / 2 + 3);
+        }
+
+        if (length % 2 != 0) {
+            printSmileyLine((int) Math.ceil((double) length / 2) + 3);
+            System.out.println("");
+            printCharacterLine(input, 2);
+            System.out.println("");
+            printSmileyLine((int) Math.ceil((double) length / 2) + 3);
+        }
+    }
 }
+
+
 
