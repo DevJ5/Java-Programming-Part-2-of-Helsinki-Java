a813e26978845d3885a97246b4eeb1f112099f0d
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-08 10:40:48 +0100

    Initial commit

---
+++ week7-week7_01.Smileys/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Smileys smiley = new Smileys("Happy");
+
+        System.out.println(smiley.name);
+    }
+}
---
+++ week7-week7_01.Smileys/src/Smileys.java
@@ -0,0 +1,18 @@
+
+public class Smileys {
+    private String name;
+
+    public Smileys(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        // Test your method at least with the following
+        // printWithSmileys("Method");
+        // printWithSmileys("Beerbottle");
+        // printWithSmileys("Interface")
+        // okay;
+    }
+
+}
+
