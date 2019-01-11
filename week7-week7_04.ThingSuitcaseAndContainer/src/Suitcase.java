248d6aebf73a6d337dd834bc50ac75f46ed65431
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-11 18:55:03 +0100

    Fixed suitcases

--- week7-week7_04.ThingSuitcaseAndContainer/src/Suitcase.java
+++ week7-week7_04.ThingSuitcaseAndContainer/src/Suitcase.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Suitcase {
+    private int maxWeight;
+    private ArrayList<Thing> things;
+
+    public Suitcase(int maxWeight) {
+        this.maxWeight = maxWeight;
+        this.things = new ArrayList<Thing>();
+    }
+
+    public void addThing(Thing thing) {
+        if(thing.getWeight() + totalWeight() <= maxWeight) this.things.add(thing);
+    }
+
+    public int totalWeight(){
+        int weight = 0;
+        for(Thing thing : things) {
+            weight += thing.getWeight();
+        }
+
+        return weight;
+    }
+
+    public void printThings(){
+        for (Thing thing : things) {
+            System.out.println(thing);
+        }
+    }
+
+    public Thing heaviestThing(){
+        if(things.size() == 0) return null;
+        Thing heaviestSoFar = things.get(0);
+
+        for (Thing thing : things) {
+            if(heaviestSoFar.getWeight() < thing.getWeight()) heaviestSoFar = thing;
+        }
+
+        return heaviestSoFar;
+    }
+
+    @Override
+    public String toString() {
+        if(things.size() == 0) return "empty (0kg)";
+        if(things.size() == 1)
+            return things.size() + " thing (" + this.totalWeight() + " kg";
+
+        return things.size() + " things (" + this.totalWeight() + " kg";
+    }
 }
--- week7-week7_04.ThingSuitcaseAndContainer/src/Thing.java
+++ week7-week7_04.ThingSuitcaseAndContainer/src/Thing.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Thing {
+    private String name;
+    private int weight;
+
+    public Thing(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString(){
+        return this.name + " (" + this.weight + " kg)";
+    }
 }
