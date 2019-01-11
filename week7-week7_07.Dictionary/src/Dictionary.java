43d0d693e33f8962596f18ef00db9e7650c32712
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-11 18:51:16 +0100

    Hash maps

--- week7-week7_07.Dictionary/src/Dictionary.java
+++ week7-week7_07.Dictionary/src/Dictionary.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Dictionary {
+    private HashMap<String, String> dictionary;
+
+    public Dictionary(){
+        this.dictionary = new HashMap<String, String>();
+    }
+
+    public void add(String word, String translation) {
+        this.dictionary.put(word, translation);
+    }
+
+    public String translate(String word) {
+        return this.dictionary.get(word);
+    }
+
+    public int amountOfWords(){
+        return this.dictionary.size();
+    }
+
+    public ArrayList<String> translationList(){
+        ArrayList<String> list = new ArrayList<String>();
+        for(String key : this.dictionary.keySet()) {
+            list.add(key + " = " + this.dictionary.get(key));
+        }
+
+        return list;
+    }
 }
--- week7-week7_07.Dictionary/src/Main.java
+++ week7-week7_07.Dictionary/src/Main.java
@@ -4,5 +4,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String input = "add\n" +  "something\n" + "sumzthing\n" + "translate\n" + "something\n" + "quit";
+        Scanner reader = new Scanner(input);
+        Dictionary dict = new Dictionary();
+        TextUserInterface UI = new TextUserInterface(reader, dict);
+
+        UI.start();
+
     }
 }
