345707aeb95db80259250c3a0559cea2a7e6c74f
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-18 19:24:59 +0100

    Airport

--- week7-week7_08.Airport/src/Main.java
+++ week7-week7_08.Airport/src/Main.java
@@ -1,6 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Write your main program here. Implementing your own classes will be useful.
+        // Airport.
+        /* User requirements:
+            Planes have an ID and a capacity.
+            Flights have a plane, a departure code and a destination code.
+            The same plane go on multiple flights.
+            The application has 2 panels.
+                - Airport panel, where flight and plane information are entered.
+                - Flight service panel, it has has 3 actions: printing planes, printing flights, printing airplane info.
+            The user can exit the app by entering 'x'.
+            If a command is not recognized, user is prompted again.
+            The program starts in the main.
+         */
+
+
     }
 }
