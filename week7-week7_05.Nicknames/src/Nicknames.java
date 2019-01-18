fbd5d0cdff2e5fda25c059b40caf43674d7dea03
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-18 14:30:39 +0100

    Nicknames

--- week7-week7_05.Nicknames/src/Nicknames.java
+++ week7-week7_05.Nicknames/src/Nicknames.java
@@ -1,11 +1,21 @@
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations requested in the assignment here!
+        Map<String, String> names = new HashMap<String, String>();
+
+        names.put("matti", "mage");
+        names.put("mikael", "mixu");
+        names.put("arto", "arppa");
+
+        System.out.println(names.get("mikael"));
+
     }
 
 }
