8b85cf0d204810ef06002233ab0b0197633949d8
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-22 17:24:58 +0100

    Exercise 24

--- week9-week9_24.MultipleEntryDictionary/src/dictionary/Main.java
+++ week9-week9_24.MultipleEntryDictionary/src/dictionary/Main.java
@@ -1,7 +1,18 @@
 package dictionary;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         // Test your dictionary here
+        PersonalMultipleEntryDictionary dict = new PersonalMultipleEntryDictionary();
+        dict.add("kuusi", "six");
+        dict.add("kuusi", "spruce");
+        System.out.println(dict.translate("kuusi"));
+        dict.remove("kuusi");
+        System.out.println(dict.translate("kuusi"));
     }
 }
---
+++ week9-week9_24.MultipleEntryDictionary/src/dictionary/PersonalMultipleEntryDictionary.java
@@ -0,0 +1,33 @@
+package dictionary;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PersonalMultipleEntryDictionary implements MultipleEntryDictionary {
+    private Map<String, Set<String>> translations;
+
+    public PersonalMultipleEntryDictionary() {
+        this.translations = new HashMap<String, Set<String>>();
+    }
+
+    @Override
+    public void add(String word, String entry) {
+        if(!translations.containsKey(word)) {
+            translations.put(word, new HashSet<String>());
+        }
+
+        translations.get(word).add(entry);
+    }
+
+    @Override
+    public Set<String> translate(String word) {
+        return translations.get(word);
+    }
+
+    @Override
+    public void remove(String word) {
+        translations.remove(word);
+    }
+}
