d670ebc7392f8e05aa72aaab4b6ff77e7350874d
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2019-01-27 13:45:30 +0100

    User interfaces

--- week11-week11_37.Survey/src/survey/UserInterface.java
+++ week11-week11_37.Survey/src/survey/UserInterface.java
@@ -1,7 +1,7 @@
 package survey;
 
 import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.*;
 import javax.swing.*;
 
 public class UserInterface implements Runnable {
@@ -11,8 +11,38 @@ public class UserInterface implements Runnable {
     @Override
     public void run() {
         // Create your app here
+        frame = new JFrame("Survey");
+        frame.setPreferredSize(new Dimension(200, 300));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
+    public void createComponents(Container container) {
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+
+        container.add(new JLabel("Are you?"));
+        container.add(new JCheckBox("Yes!"));
+        container.add(new JCheckBox("No!"));
+        container.add(new JLabel("Why?"));
+
+        JRadioButton noReason = new JRadioButton("No reason.");
+        JRadioButton funReason = new JRadioButton("Because it is fun!");
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(noReason);
+        buttonGroup.add(funReason);
+
+        container.add(noReason);
+        container.add(funReason);
+
+        container.add(new JButton("Done!"));
+    }
 
     public JFrame getFrame() {
         return frame;
--- week11-week11_38.NoticeBoard/src/noticeboard/NoticeBoard.java
+++ week11-week11_38.NoticeBoard/src/noticeboard/NoticeBoard.java
@@ -11,8 +11,29 @@ public class NoticeBoard implements Runnable {
 
     @Override
     public void run() {
+        frame = new JFrame("Title");
+        frame.setPreferredSize(new Dimension(400, 300));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private void createComponents(Container container) {
+        GridLayout layout =  new GridLayout(3,1);
+        container.setLayout(layout);
+
+        JTextField textField = new JTextField();
+        JButton copyBtn = new JButton("Copy!");
+        JLabel label = new JLabel();
+
+        copyBtn.addActionListener(new ActionEventListener(textField, label));
+
+        container.add(textField);
+        container.add(copyBtn);
+        container.add(label);
     }
 }
